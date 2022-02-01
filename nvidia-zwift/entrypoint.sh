701e1ff9b767c547d6bfdd5473d15af4cd5c9e0d
Kim Eik <kim@heldig.org>
2022-02-01 23:38:18 +0100

    updated zwift image to support other graphics cards

--- nvidia-zwift/entrypoint.sh
+++ nvidia-zwift/entrypoint.sh
@@ -17,25 +17,20 @@ fi
 
 if [ ! "$(ls -A .)" ] # is directory empty?
 then
-	#install dotnet > 4.7.2
+	echo "installing dotnet"
 	winetricks --unattended dotnet48 win10
-	
-	#prevents warning when starting zwift for the first time
-	winetricks --unattended dotnet20 win10
-
-	#workaround crash issue 1.21
+	echo "workaround crash issue 1.21" # https://bugs.winehq.org/show_bug.cgi?id=45871
 	winetricks --unattended d3dcompiler_47
-	
-	#install msedge
-	wget https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/d1b0e946-e654-4d81-a42a-d581b8f3c40c/MicrosoftEdgeWebView2RuntimeInstallerX64.exe
-	wine64 MicrosoftEdgeWebview2RuntimeInstallerX64.exe /silent /install
+	echo "installing webview2"
+	#wget -O MicrosoftEdgeWebview2Setup.exe https://go.microsoft.com/fwlink/p/?LinkId=2124703
+	wget -O MicrosoftEdgeWebview2Setup.exe https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/d1b0e946-e654-4d81-a42a-d581b8f3c40c/MicrosoftEdgeWebView2RuntimeInstallerX64.exe
+	wine64 MicrosoftEdgeWebview2Setup.exe /silent /install
 
-	#install zwift
+	echo "installing zwift..."
         wget https://www.nirsoft.net/utils/runfromprocess.zip
-	unzip runfromprocess.zip
+	unzip runfromprocess.zip	
 	wget https://cdn.zwift.com/app/ZwiftSetup.exe
 	wine64 ZwiftSetup.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL
-
 	# Wait for Zwift to fully install and then restart container
 	wineserver -w
 	exit 0
